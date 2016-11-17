897c948d4749fd245c7cc8732e0313ed25d5ff56
nrigano <nrigano@gmail.com>
2016-11-17 16:31:27 -0500

    Add loginViewController

---
+++ FlatironMasterpiece/LogInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LogInViewController.swift
+//  FlatironMasterpiece
+//
+//  Created by Nick Rigano on 11/17/16.
+//  Copyright © 2016 Elias Miller. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+}
