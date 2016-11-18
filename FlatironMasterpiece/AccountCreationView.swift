1e7058154e6810c0b7335ef70180f10068ab61ae
elimiller783 <emiller783@gmail.com>
2016-11-17 22:57:40 -0500

    added outlets and framework for account validation

--- FlatironMasterpiece/AccountCreationView.swift
+++ FlatironMasterpiece/AccountCreationView.swift
@@ -1,14 +1,48 @@
 //
 //  AccountCreationView.swift
-//  CreationAccount
+//  FlatironMasterpiece
 //
-//  Created by Joyce Matos on 11/16/16.
-//  Copyright © 2016 Joyce Matos. All rights reserved.
+//  Created by Elias Miller on 11/17/16.
+//  Copyright © 2016 Elias Miller. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
+struct Constants {
+    static let FIRSTNAME = "firstNameTextField"
+    static let LASTNAME = "lastNameTextField"
+    static let EMAILCONFIRMATION = "emailTextField"
+    static let PASSWORD = "password"
+    static let INDUSTRY = "industry"
+    static let JOBTITLE = "jobtitle"
+    
+}
 
+class AccountCreationView: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var firstNameEntryTextField: UITextField!
+    @IBOutlet weak var lastNameEntryTextField: UITextField!
+    @IBOutlet weak var emailEntry: UITextField!
+    @IBOutlet weak var passwordEntry: UITextField!
+    @IBOutlet weak var passwordVerification: UITextField!
+    @IBOutlet weak var industryEntry: UITextField!
+    @IBOutlet weak var jobEntry: UITextField!
+    
+    var firstNameConfirmed = false
+    var lastNameConfirmed = false
+    var emailConfirmed = false
+    var password = false
+    var industry = false
+    var jobtitle = false
+    
+//    firstNameEntry
+//    lastNameEntry
+//    emailEntry
+//    passwordEntry
+//    passwordVerification
+//    industryEntry
+//    jobEntry
+//    
     
     
-    
+}
