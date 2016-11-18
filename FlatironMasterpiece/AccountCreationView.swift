1b888d22714702f99edfd42a6ff9df163c037adf
elimiller783 <emiller783@gmail.com>
2016-11-17 23:32:40 -0500

    working on account validation

--- FlatironMasterpiece/AccountCreationView.swift
+++ FlatironMasterpiece/AccountCreationView.swift
@@ -14,6 +14,7 @@ struct Constants {
     static let LASTNAME = "lastNameTextField"
     static let EMAILCONFIRMATION = "emailTextField"
     static let PASSWORD = "password"
+    static let PASSWORDVERIFICATION = "passwordverification"
     static let INDUSTRY = "industry"
     static let JOBTITLE = "jobtitle"
     
@@ -22,11 +23,12 @@ struct Constants {
 class AccountCreationView: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var firstNameEntryTextField: UITextField!
     @IBOutlet weak var lastNameEntryTextField: UITextField!
-    @IBOutlet weak var emailEntry: UITextField!
-    @IBOutlet weak var passwordEntry: UITextField!
-    @IBOutlet weak var passwordVerification: UITextField!
-    @IBOutlet weak var industryEntry: UITextField!
-    @IBOutlet weak var jobEntry: UITextField!
+    @IBOutlet weak var emailEntryTextField: UITextField!
+    @IBOutlet weak var passwordEntryTextField: UITextField!
+    @IBOutlet weak var passwordVerificationTextField: UITextField!
+    @IBOutlet weak var industryEntryTextField: UITextField!
+    @IBOutlet weak var jobEntryTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
     
     var firstNameConfirmed = false
     var lastNameConfirmed = false
@@ -34,6 +36,49 @@ class AccountCreationView: UIViewController, UITextFieldDelegate {
     var password = false
     var industry = false
     var jobtitle = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    firstNameEntryTextField.accessibilityLabel = Constants.FIRSTNAME
+        lastNameEntryTextField.accessibilityLabel = Constants.LASTNAME
+        emailEntryTextField.accessibilityLabel = Constants.EMAILCONFIRMATION
+        passwordEntryTextField.accessibilityLabel = Constants.PASSWORD
+        passwordVerificationTextField.accessibilityLabel = Constants.PASSWORDVERIFICATION
+        industryEntryTextField.accessibilityLabel = Constants.INDUSTRY
+        jobEntryTextField.accessibilityLabel = Constants.JOBTITLE
+        
+    }
+
+    func animateFields(textField: UITextField, isInputValid: Bool) {
+        let greenColor = UIColor.green.withAlphaComponent(0.1)
+        let redColor = UIColor.red.withAlphaComponent(0.1)
+        let unchangedState = textField.transform
+        let pulseScale: CGFloat = 1.03
+        let shake = textField.frame.width * 0.02
+        
+        if isInputValid {
+            UIView.animateKeyframes(withDuration: 0.4, delay: 0, options: UIViewKeyframeAnimationOptions.calculationModeLinear, animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
+                    textField.backgroundColor = greenColor
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                    textField.transform = CGAffineTransform(scaleX: pulseScale, y: pulseScale)
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 1/3) {
+                    textField.transform = unchangedState
+                }
+                
+            }, completion: nil)
+
+        
+        
+        
+        }
+    }
+    
+    
+    
+    
     
 //    firstNameEntry
 //    lastNameEntry
