0784b16c490be21659023def087b5b60726361b9
JoyceMatos <j.matosjoyce@gmail.com>
2016-11-19 10:23:02 -0500

    Add logic to login buttons

--- FlatironMasterpiece/LogInViewController.swift
+++ FlatironMasterpiece/LogInViewController.swift
@@ -13,19 +13,40 @@ import FirebaseAuth
 
 class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    var loginLabel = UILabel()
+    var loginEmail = UITextField()
+    var loginPassword = UITextField()
+    var registerButton = UIButton()
+    var forgotPasswordButton = UIButton()
+    var loginButton = UIButton()
+    var fbLoginButton: FBSDKLoginButton =  FBSDKLoginButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var loginLabel = UILabel()
-        var loginEmail = UITextField()
-        var loginPassword = UITextField()
-        var registerButton = UIButton()
-        var forgotPasswordButton = UIButton()
-        var loginButton = UIButton()
-        var fbLoginButton: FBSDKLoginButton =  FBSDKLoginButton()
-
         
-        //need to add LOGIN SCREEN label at very top of view
+        createViews()
+    
+        
+        // Logic for FB Login Button 
+        
+        FIRAuth.auth()?.addStateDidChangeListener { auth, user in
+            if let user = user {
+                // User is signed in.
+                // Move to next screen
+                // Add logout button to user's settings screen
+            } else {
+                // No user is signed in.
+            }
+        }
+        
+    
+    
+    }
+    
+    
+    func createViews() {
+        
+        // login label
         view.addSubview(loginLabel)
         loginLabel.text = "Log In"
         loginLabel.textAlignment = .center
@@ -47,6 +68,7 @@ class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
         loginEmail.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.66).isActive = true
         loginEmail.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
+        // login password textfield
         view.addSubview(loginPassword)
         loginPassword.placeholder = "  App Password"
         loginPassword.layer.borderWidth = 1
@@ -59,9 +81,10 @@ class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
         loginPassword.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
         
-        
+        // login button
         view.addSubview(loginButton)
         loginButton.setTitle("Log In", for: UIControlState.normal)
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         loginButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         loginButton.topAnchor.constraint(equalTo: loginPassword.bottomAnchor, constant: 20).isActive = true
@@ -69,9 +92,10 @@ class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
         loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.66).isActive = true
         loginButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
-        
+        // Register Button
         view.addSubview(registerButton)
         registerButton.setTitle("Register?", for: UIControlState.normal)
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
         registerButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
         registerButton.translatesAutoresizingMaskIntoConstraints = false
         registerButton.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20).isActive = true
@@ -79,18 +103,16 @@ class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
         registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.66).isActive = true
         registerButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         
-
+        
         // FB Log in Button
         fbLoginButton.readPermissions = ["public_profile", "email", "user_friends"]
         fbLoginButton.delegate = self
         view.addSubview(fbLoginButton)
         fbLoginButton.center = self.view.center
         fbLoginButton.topAnchor.constraint(equalTo: registerButton.bottomAnchor, constant: 20).isActive = true
-
         
         
-        
-        
+        // Forgot Password Button
         view.addSubview(forgotPasswordButton)
         forgotPasswordButton.setTitle("Forgot Password?", for: UIControlState.normal)
         forgotPasswordButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
@@ -99,13 +121,59 @@ class LogInViewController: UIViewController, FBSDKLoginButtonDelegate {
         forgotPasswordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         forgotPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.66).isActive = true
         forgotPasswordButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
+
         
-    
-    
-    
-    
     }
     
+
+
+
+    func loginButtonTapped(sender: UIButton!) {
+    
+    if self.loginEmail.text == "" || loginPassword.text == "" {
+        
+        // TODO: - Create action
+        
+        print("Enter email or password")
+        
+        
+    } else {
+        
+        
+        // TODO: - Unwrap login email and password
+        
+        FIRAuth.auth()?.signIn(withEmail: loginEmail.text!, password: loginPassword.text!, completion: { (user, error) in
+            
+            if error == nil {
+                print("Successful Log In")
+                self.loginButton.alpha = 1.0
+                
+            }
+                
+            else {
+                
+                print(error?.localizedDescription)
+                
+            }
+            
+            
+            
+            
+        })
+        
+        
+    }
+
+    
+    
+}
+
+    func registerButtonTapped(sender: UIButton!) {
+        
+        // Send to Account Creation Page
+        
+    }
+
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
         
         print("=================\(#function)=============\n\n\n")
