58291136cbd42d99ca15076f56d2c83d182b9163
Erica Millado <ericamillado@gmail.com>
2016-11-22 16:11:05 -0500

    Changed app icon

---
+++ FlatironMasterpiece/ShakeInstructionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShakeInstructionViewController.swift
+//  FlatironMasterpiece
+//
+//  Created by Erica Millado on 11/22/16.
+//  Copyright © 2016 Elias Miller. All rights reserved.
+//
+
+import UIKit
+
+class ShakeInstructionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
