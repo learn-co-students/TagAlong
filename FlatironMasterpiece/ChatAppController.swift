442ce10c12871109449e221d2dd5af0b05889d94
JoyceMatos <j.matosjoyce@gmail.com>
2016-11-23 11:34:30 -0500

    Restructure firebase

---
+++ FlatironMasterpiece/ChatAppController.swift
@@ -0,0 +1,31 @@
+//
+//  ChatAppViewController.swift
+//  FlatironMasterpiece
+//
+//  Created by Joyce Matos on 11/23/16.
+//  Copyright © 2016 Elias Miller. All rights reserved.
+//
+
+import UIKit
+
+class ChatAppViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
--- FlatironMasterpiece/ChatViewController.swift
+++ FlatironMasterpiece/ChatViewController.swift
@@ -17,7 +17,7 @@ class ChatViewController: JSQMessagesViewController {
     
     // Stores user's name and match
     var userName: String?
-    var matchChannel = [Match]()
+//    var matchChannel = [Match]()
     
     //
     //var ref = FIRDatabase.database().reference().root
@@ -45,22 +45,24 @@ class ChatViewController: JSQMessagesViewController {
            // self.senderId = FIRAuth.auth()?.currentUser?.uid
            // self.senderDisplayName = FIRAuth.auth()?.currentUser?.uid
             
-            self.createMatchChannel()
+            self.setupChatDatabase()
 //        })
         
     }
     
-    func createMatchChannel() {
+    func setupChatDatabase() {
+        
+        // Match branch
         
         let newMatchRef = matchRef.childByAutoId()
-        
         let messageRef = newMatchRef.child("Messages")
         
         print(messageRef)
         
         let convo = [
             "name" : senderId,
-            "message" : "Hello"     // This will be taken from textfield
+            "message" : "Hello",     // This will be taken from textfield
+            "timestamp" : "8:00"    // This will be the time the chat started
             ]
         
         messageRef.setValue(convo, withCompletionBlock: { error, ref in
@@ -71,41 +73,7 @@ class ChatViewController: JSQMessagesViewController {
             
         })
         
-        //let uniqueMessageID = UUID().uuidString
-        
-//        let convo = [uniqueMessageID : ["name" : "Jim", "message": "YOLO!!!", "Time" : "10000"]]
-        
-        
-        
-     //   messageRef.updateChildValues(convo)
-        
-    }
-    
-//    
-//    deinit {
-//        if let refHandle = matchRefHandle {
-//            matchRef.removeObserver(withHandle: refHandle)
-//        }
-//    }
-    
-    // MARK: Firebase related methods
-    
-    //You call observe:with: on your channel reference, storing a handle to the reference. This calls the completion block every time a new channel is added to your database.
-    //The completion receives a FIRDataSnapshot (stored in snapshot), which contains the data and other helpful methods.
-    //You pull the data out of the snapshot and, if successful, create a Channel model and add it to your channels array.
-//    private func observeChannels() {
-//        // Use the observe method to listen for new
-//        // channels being written to the Firebase DB
-//        matchRefHandle = matchRef.observe(.childAdded, with: { (snapshot) -> Void in // 1
-//            let channelData = snapshot.value as! [String : AnyObject] // 2
-//            let id = snapshot.key
-//            if let name = channelData["name"] as! String!, name.characters.count > 0 { // 3
-//                self.matchChannel.append(Match(id: id, name: name))
-//            } else {
-//                print("Error! Could not decode channel data")
-//            }
-//        })
-//    }
 
+    }
     
 }
