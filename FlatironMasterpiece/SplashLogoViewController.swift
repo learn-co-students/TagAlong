b1646ebf29589264b6f66dedf7c7249b44697b88
Erica Millado <ericamillado@gmail.com>
2016-12-01 19:47:10 -0500

    About to pull from master

---
+++ FlatironMasterpiece/SplashLogoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SplashLogoViewController.swift
+//  FlatironMasterpiece
+//
+//  Created by Erica Millado on 12/1/16.
+//  Copyright © 2016 Elias Miller. All rights reserved.
+//
+
+import UIKit
+
+class SplashLogoViewController: UIViewController {
+    
+    var logoImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = phaedraOrange
+        // Do any additional setup after loading the view.
+    }
+
+    func setupImage() {
+        logoImageView.image = UIImageView(image: <#T##UIImage?#>)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
