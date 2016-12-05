19ab9244a453f8d844f01a4cce2eb28f5f8ae6a5
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-05 16:32:03 -0500

    Test on user tagalong and current tagalong is successful

--- FlatironMasterpiece/TagAlongViewController.swift
+++ FlatironMasterpiece/TagAlongViewController.swift
@@ -148,17 +148,4 @@ class TagAlongViewController: UIViewController, UITableViewDataSource, UITableVi
     
     
     
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
