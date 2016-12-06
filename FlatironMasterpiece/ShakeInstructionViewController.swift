e7421e24d8b9b8c357a1eb83c9b07b7ae70e12e2
elimiller783 <emiller783@gmail.com>
2016-12-05 22:54:21 -0500

    added sound

--- FlatironMasterpiece/ShakeInstructionViewController.swift
+++ FlatironMasterpiece/ShakeInstructionViewController.swift
@@ -12,25 +12,56 @@ import AVFoundation
 class ShakeInstructionViewController: UIViewController {
  
     var shakeView: ShakeView!
-
+    var shakeNosie: AVAudioPlayer?
+    var vview: UIView!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        let vview = ShakeView()
+        vview = ShakeView()
         view.backgroundColor = UIColor.blue
         if view == vview {
             vibrate()
+            playSound()
         }
-        
+        playSound()
+
    
     }
 
+    
     override func loadView() {
         super.loadView()
         shakeView = ShakeView()
         self.view = shakeView
     }
     
+    
+    func setupAudioPlayerWithFile(file: String, type: String) -> AVAudioPlayer? {
+        let path = Bundle.main.path(forResource: file as String, ofType: type as String)
+        let url = URL(fileURLWithPath: path!)
+        
+        var audioPlayer: AVAudioPlayer?
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("Nothing to play")
+        }
+        return audioPlayer
+    }
+    
+    func loadPlayer() {
+        if let shakeNosie = self.setupAudioPlayerWithFile(file: "SprayShake", type: "mp3") {
+            self.shakeNosie = shakeNosie
+            
+        }
+        self.shakeNosie?.volume = 1.0
+        self.shakeNosie?.play()
+    }
+    
+    func playSound() {
+       loadPlayer()
+    }
     func vibrate() {
         AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
     }
