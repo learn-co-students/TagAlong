e7a29501d00f179e2125c393afea419d7a587364
Erica Millado <ericamillado@gmail.com>
2016-12-06 10:26:53 -0500

    Set app to portrait only

--- FlatironMasterpiece/AppDelegate.swift
+++ FlatironMasterpiece/AppDelegate.swift
@@ -36,25 +36,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         //MARK: - sets up the navigation controller for our app
 
-//        navController = UINavigationController()
-//        var loginVC = LogInViewController()
-//        let frame = UIScreen.main.bounds
-//        window = UIWindow(frame: frame)
-//        self.navController?.pushViewController(loginVC, animated: false)
-//        self.window?.rootViewController = navController
-//        self.window?.backgroundColor = phaedraDarkGreen
-//        self.window?.makeKeyAndVisible()
+        navController = UINavigationController()
+        var loginVC = LogInViewController()
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        self.navController?.pushViewController(loginVC, animated: false)
+        self.window?.rootViewController = navController
+        self.window?.backgroundColor = phaedraDarkGreen
+        self.window?.makeKeyAndVisible()
 
         //MARK: - sets the initial view controller
 
-       let initialViewController = ShakeInstructionViewController()
-
-       let frame = UIScreen.main.bounds
-        window = UIWindow(frame: frame)
-        if let window = window {
-            window.rootViewController = initialViewController
-            window.makeKeyAndVisible()
-        }
+//       let initialViewController = ShakeInstructionViewController()
+//
+//       let frame = UIScreen.main.bounds
+//        window = UIWindow(frame: frame)
+//        if let window = window {
+//            window.rootViewController = initialViewController
+//            window.makeKeyAndVisible()
+//        }
 
         return true
     }
