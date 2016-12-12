36e30586064559903bf25002274fca2fe20e7f32
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-12 11:13:32 -0500

    Add guest info to labels

--- FlatironMasterpiece/SearchingForTagAlongViewController.swift
+++ FlatironMasterpiece/SearchingForTagAlongViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class SearchingForTagAlongViewController: UIViewController {
-
+    
     let store = FirebaseManager.shared
-
+    
     let searchingLabel: UILabel = UILabel()
     var searchAgainButton: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 30))
     var beTagAlongGuestButton: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 30))
@@ -22,10 +22,10 @@ class SearchingForTagAlongViewController: UIViewController {
     var guestName = String()
     var guestJob = String()
     var firstTimeLoaded = true
-
+    
     var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = phaedraBeige
@@ -34,16 +34,16 @@ class SearchingForTagAlongViewController: UIViewController {
         print(FirebaseManager.currentUser)
         print(firstTimeLoaded)
     }
-
+    
     override func loadView() {
         super.loadView()
         setupViews()
-
-
+        
+        
     }
-
+    
     func setupViews() {
-
+        
         //NOTE: - Searching for tagalong label
         view.addSubview(searchingLabel)
         searchingLabel.font = UIFont(name: "OpenSans-Semibold", size: 25.0)
@@ -56,7 +56,7 @@ class SearchingForTagAlongViewController: UIViewController {
         searchingLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
         searchingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         searchingLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
-
+        
         //NOTE: - Activity Indicator
         view.addSubview(activityIndicator)
         activityIndicator.color = phaedraYellow
@@ -65,13 +65,13 @@ class SearchingForTagAlongViewController: UIViewController {
         activityIndicator.topAnchor.constraint(equalTo: searchingLabel.bottomAnchor, constant: 50).isActive = true
         activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         activityIndicator.startAnimating()
-
+        
         //NOTE: - Search for a tagalong button
         view.addSubview(searchAgainButton)
         searchAgainButton.backgroundColor = phaedraOrange
         searchAgainButton.layer.cornerRadius = 5
         searchAgainButton.setTitle("Choose Another Restaurant", for: UIControlState.normal)
-//        searchAgainButton.setTitle("Tutorial Played", for: .highlighted)
+        //        searchAgainButton.setTitle("Tutorial Played", for: .highlighted)
         searchAgainButton.titleLabel?.font = UIFont(name: "OpenSans-Semibold", size: 17.0)
         searchAgainButton.titleLabel?.textAlignment = .center
         searchAgainButton.translatesAutoresizingMaskIntoConstraints = false
@@ -81,13 +81,13 @@ class SearchingForTagAlongViewController: UIViewController {
         searchAgainButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.65).isActive = true
         searchAgainButton.addTarget(self, action: #selector(returnToDeckView), for: .touchUpInside)
         searchAgainButton.setTitleColor(phaedraYellow, for: .highlighted)
-
+        
         //NOTE: - Be a tagalong button
         view.addSubview(beTagAlongGuestButton)
         beTagAlongGuestButton.backgroundColor = phaedraOrange
         beTagAlongGuestButton.layer.cornerRadius = 5
         beTagAlongGuestButton.setTitle("Be A Tag Along Instead", for: UIControlState.normal)
-//        beTagAlongGuestButton.setTitle("Tutorial Played", for: .highlighted)
+        //        beTagAlongGuestButton.setTitle("Tutorial Played", for: .highlighted)
         beTagAlongGuestButton.titleLabel?.font = UIFont(name: "OpenSans-Semibold", size: 17.0)
         beTagAlongGuestButton.titleLabel?.textAlignment = .center
         beTagAlongGuestButton.translatesAutoresizingMaskIntoConstraints = false
@@ -95,10 +95,10 @@ class SearchingForTagAlongViewController: UIViewController {
         beTagAlongGuestButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         beTagAlongGuestButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.05).isActive = true
         beTagAlongGuestButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.65).isActive = true
-//        beTagAlongGuestButton.leadingAnchor.constraint(equalTo: searchAgainButton.trailingAnchor, constant: 5).isActive = true
+        //        beTagAlongGuestButton.leadingAnchor.constraint(equalTo: searchAgainButton.trailingAnchor, constant: 5).isActive = true
         beTagAlongGuestButton.addTarget(self, action: #selector(seeOtherTagAlongs), for: .touchUpInside)
         beTagAlongGuestButton.setTitleColor(phaedraYellow, for: .highlighted)
-//
+        //
         //NOTE: - Label for tagalong found
         view.addSubview(tagAlongFoundLabel)
         tagAlongFoundLabel.font = UIFont(name: "OpenSans-Semibold", size: 25.0)
@@ -112,13 +112,12 @@ class SearchingForTagAlongViewController: UIViewController {
         tagAlongFoundLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         tagAlongFoundLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
         tagAlongFoundLabel.isHidden = true
-
+        
         //NOTE: - Tag along details
         view.addSubview(tagAlongDetailLabel)
         tagAlongDetailLabel.font = UIFont(name: "OpenSans-Light", size: 20.0)
         tagAlongDetailLabel.lineBreakMode = .byWordWrapping
         tagAlongDetailLabel.numberOfLines = 0
-        tagAlongDetailLabel.text = "iOS Developer, Christina, would like to tag along with you."
         tagAlongDetailLabel.textColor = phaedraOrange
         tagAlongDetailLabel.textAlignment = .center
         tagAlongDetailLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -126,9 +125,9 @@ class SearchingForTagAlongViewController: UIViewController {
         tagAlongDetailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         tagAlongDetailLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
         tagAlongDetailLabel.isHidden = true
-
+        
         //NOTE: - Accept tagalong button
-
+        
         view.addSubview(acceptButton)
         acceptButton.backgroundColor = phaedraOrange
         acceptButton.layer.cornerRadius = 5
@@ -143,7 +142,7 @@ class SearchingForTagAlongViewController: UIViewController {
         acceptButton.setTitleColor(phaedraYellow, for: .normal)
         acceptButton.setTitleColor(phaedraLightGreen, for: .highlighted)
         acceptButton.isHidden = true
-
+        
         //NOTE: - Deny tagalong button
         view.addSubview(denyButton)
         denyButton.backgroundColor = phaedraOrange
@@ -159,110 +158,101 @@ class SearchingForTagAlongViewController: UIViewController {
         denyButton.setTitleColor(phaedraYellow, for: .normal)
         denyButton.setTitleColor(phaedraLightGreen, for: .highlighted)
         denyButton.isHidden = true
-
+        
     }
-
-
+    
+    
     func returnToDeckView() {
         print("User wants to return to deck view")
         let shakeInstructionVC = ShakeInstructionViewController()
         self.navigationController?.pushViewController(shakeInstructionVC, animated: true)
     }
-
-
+    
+    
     func seeOtherTagAlongs() {
         print("User wants to see other tagalongs")
         let tagAlongsVC = TagAlongViewController()
         self.navigationController?.pushViewController(tagAlongsVC, animated: true)
     }
-
+    
     func acceptTagalongAction() {
         print("getting hit")
         print(store.guestID)
         guard let acceptedGuestID = store.guestID else { print("leaving function");return }
         print(acceptedGuestID)
-
+        
         store.acceptTagalong(guestID: acceptedGuestID) { (currentTagAlongKey) in
             self.store.updateGuestWithTagAlongKey(tagAlongkey: currentTagAlongKey)
-
+            
             // Add true value to hidden node
             self.store.hideTagalong(for: currentTagAlongKey)
-
+            
             let tabBarVC = TabBarController()
             tabBarVC.tagAlong = currentTagAlongKey
-
+            
             self.navigationController?.pushViewController(tabBarVC, animated: true)
         }
     }
-
-
+    
+    
     func denyTagalongAction() {
-
+        
         guard let deniedGuestID = store.guestID else { print("leaving function");return }
         print(deniedGuestID)
-
+        
         store.denyTagalong(guestID: deniedGuestID)
-
+        
     }
-
-
+    
+    
     func observeTagalongRequests() {
-
+        
         store.observeTagalongRequests { (snapshot) in
-
+            
             //Prevents old tagalongs to appear if
             //            if self.firstTimeLoaded { self.firstTimeLoaded = false; return }
-
+            
             // Only detects recents tagalongs
             //            if !self.firstTimeLoaded {
-
+            
             //Alert user of new tag along
             print("\n ==============Getting called.===============")
-
+            
             guard snapshot != nil else { print("snapshot is nil");return }
-
-                //Show labels and buttons
-                self.acceptButton.isHidden = false
-                self.denyButton.isHidden = false
-                self.tagAlongFoundLabel.isHidden = false
-                self.tagAlongDetailLabel.isHidden = false
-
-                                // Grab info from guest
-
-//                guard let tagalongID = self.store.selectedTagAlongID else { return }
-
-                self.store.guestID = snapshot?.key
-
-//                self.store.createGuestFrom(tagalong: self.store.selectedTagAlongID, completion: { (guest) in
-//
-//                    print("\(guest.firstName), \(guest.jobTitle), would like to tag along with you at 'restaurant'. Would you like them to tag along?")
-//
-//
-//
-//                })
-
+            
+            //Show labels and buttons
+            self.acceptButton.isHidden = false
+            self.denyButton.isHidden = false
+            self.tagAlongFoundLabel.isHidden = false
+            self.tagAlongDetailLabel.isHidden = false
+            self.searchingLabel.isHidden = true
+            self.beTagAlongGuestButton.isHidden = true
+            self.searchAgainButton.isHidden = true
+            
+            // Grab info from guest
+            self.store.guestID = snapshot?.key
+            
             guard let guestID = self.store.guestID else { print("no guest info");return }
-
+            
             self.store.createGuest(from: guestID , completion: { (guest) in
-
+                
                 self.guestName = guest.firstName
                 self.guestJob = guest.jobTitle
-                self.tagAlongFoundLabel.text = "Match: \(self.guestName), \(self.guestJob)"
-
+                self.tagAlongDetailLabel.text = "\(self.guestJob), \(self.guestName), would like to tag along with you."
+                
                 print(guest.firstName)
                 print(guest.jobTitle)
-
+                
             })
-
-                print("\n\n")
-
-                print("We have a new tag along person. \(snapshot)")
-//            }
-
-
-
-
+            
+            print("\n\n")
+            
+            print("We have a new tag along person. \(snapshot)")
+            
+            
+            
+            
         }
     }
-
+    
 }
