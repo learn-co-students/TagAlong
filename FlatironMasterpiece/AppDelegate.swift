8a1cda96df6df8c69914d7155f96df52f2315367
Erica Millado <ericamillado@gmail.com>
2016-12-14 00:48:40 -0500

    Fixed formatting on onboardingVC

--- FlatironMasterpiece/AppDelegate.swift
+++ FlatironMasterpiece/AppDelegate.swift
@@ -36,19 +36,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
          navController = UINavigationController()
 
-
-
-
-
 //        let initialViewController = LogInViewController()
-      var loginVC = LogInViewController()
-//  
-//        //      var onboardingVC = OnboardingViewController()
+//      var loginVC = LogInViewController()
+//
+              var onboardingVC = OnboardingViewController()
 
         let frame = UIScreen.main.bounds
         window = UIWindow(frame: frame)
-      self.navController?.pushViewController(loginVC, animated: false)
- //       self.navController?.pushViewController(onboardingVC, animated: true)
+//      self.navController?.pushViewController(loginVC, animated: false)
+        self.navController?.pushViewController(onboardingVC, animated: true)
 
         self.window?.rootViewController = navController
         self.window?.backgroundColor = phaedraDarkGreen
